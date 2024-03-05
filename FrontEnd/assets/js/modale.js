c1ca272a1d10169904db552028863bbe72226314
Magali ERRAMCH <erramchmagali@gmail.com>
2024-03-05 15:19:50 +0100

    mise a jour modale et  index

--- FrontEnd/assets/js/modale.js
+++ FrontEnd/assets/js/modale.js
@@ -252,3 +252,101 @@ async function refreshPage(i){
     const projet = document.querySelector(`.js-projet-${i}`);
     projet.style.display = "none";
 }
+// 4-/ GESTION BOITE MODALE AJOUT PROJET ///
+
+
+// Ouverture modale projet
+let modaleProjet = null;
+const openModaleProjet = function(e) {
+    e.preventDefault()
+    modaleProjet = document.querySelector(e.target.getAttribute("href"))
+
+    modaleProjet.style.display = null
+    modaleProjet.removeAttribute("aria-hidden")
+    modaleProjet.setAttribute("aria-modal", "true")
+
+    // Apl fermeture modale
+    modaleProjet.addEventListener("click", closeModaleProjet)
+    modaleProjet.querySelector(".js-modale-close").addEventListener("click", closeModaleProjet)
+    modaleProjet.querySelector(".js-modale-stop").addEventListener("click", stopPropagation)
+
+    modaleProjet.querySelector(".js-modale-return").addEventListener("click", backToModale)
+};
+
+
+// Fermeture  modale projet
+const closeModaleProjet = function(e) {
+    if (modaleProjet === null) return
+
+    modaleProjet.setAttribute("aria-hidden", "true")
+    modaleProjet.removeAttribute("aria-modal")
+
+    modaleProjet.querySelector(".js-modale-close").removeEventListener("click", closeModaleProjet)
+    modaleProjet.querySelector(".js-modale-stop").removeEventListener("click", stopPropagation)
+
+    modaleProjet.style.display = "none"
+    modaleProjet = null
+    
+    closeModale(e)
+};
+
+// Retour modale admin
+const backToModale = function(e) {
+    e.preventDefault()
+    modaleProjet.style.display = "none"
+    modaleProjet = null
+    modaleProjets(dataAdmin)
+};
+//5-/ GESTION AJOUT D'UN PROJET        ///
+
+const btnAjouterProjet = document.querySelector(".js-add-work");
+btnAjouterProjet.addEventListener("click", addWork);
+
+// Ajouter un projet
+async function addWork(event) {
+    event.preventDefault();
+
+    const title = document.querySelector(".js-title").value;
+    const categoryId = document.querySelector(".js-categoryId").value;
+    const image = document.querySelector(".js-image").files[0];
+
+
+    if (title === "" || categoryId === "" || image === undefined) {
+        alert("Merci de remplir tous les champs");
+        return;
+    } else if (categoryId !== "1" && categoryId !== "2" && categoryId !== "3") {
+        alert("Merci de choisir une catégorie valide");
+        return;
+        } else {
+    try {
+        const formData = new FormData();
+        formData.append("title", title);
+        formData.append("category", categoryId);
+        formData.append("image", image);
+
+        const response = await fetch("http://localhost:5678/api/works", {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            body: formData,
+        });
+
+        if (response.status === 201) {
+            alert("Projet ajouté avec succès :)");
+            modaleProjets(dataAdmin);
+            backToModale(event);
+            generationProjets(data, null);
+            
+        } else if (response.status === 400) {
+            alert("Merci de remplir tous les champs");
+        } else if (response.status === 500) {
+            alert("Erreur serveur");
+        } else if (response.status === 401) {
+            alert("Vous n'êtes pas autorisé à ajouter un projet");
+            window.location.href = "login.html";
+    }}
+
+    catch (error) {
+        console.log(error);
+}}}
