0327bca65d1b622639a38f794b2042c4c39fa99f
Magali ERRAMCH <erramchmagali@gmail.com>
2024-04-06 22:18:17 +0200

    mise a jour logout

--- FrontEnd/assets/js/conexion.js
+++ FrontEnd/assets/js/conexion.js
@@ -1,39 +1,31 @@
 // Variable à afficher pour le mode éditeur
 const modeEdition = document.querySelector(".admin__rod");
-console.log(modeEdition)
+console.log(modeEdition);
 const editBtn = document.querySelectorAll(".modifier");
-const logout = document.querySelector('[href="login.html"]');
+const logout = document.getElementById("login");
 const filters = document.querySelectorAll("#category");
 
 // Si nous avons récupéré le token
 if (localStorage.getItem("token")) {
-    modeEdition.style.display = "flex";
+  modeEdition.style.display = "flex";
 
-    //const logo = document.querySelector("#logo");
-    //logo.style.paddingTop = "25px";
-    //logo.style.fontSize = "17px";
+  for (let i = 0; i < filters.length; i++) {
+    filters[i].style.display = "none";
+  }
 
-    //const navHeader = document.querySelector("#navHeader");
-    //navHeader.style.paddingTop = "25px";
+  for (let i = 0; i < editBtn.length; i++) {
+    editBtn[i].style.display = "flex";
+  }
 
-    for (let i = 0; i < filters.length; i++) {
-        filters[i].style.display = "none";
-    }
-    
-    for (let i = 0; i < editBtn.length; i++) {
-        editBtn[i].style.display = "flex";
-    }
+  // Changer login en logout
+  logout.textContent = "logout";
+  logout.setAttribute("href", "#");
 
-    // Changer login en logout
-    logout.textContent = "logout";
-    logout.setAttribute("href", "#");
+  // Lorque l'on clic sur logout cela déconnecte l'utilisateur
+  logout.addEventListener("click", (event) => {
+    event.preventDefault();
 
-    // Lorque l'on clic sur logout cela déconnecte l'utilisateur
-    logout.addEventListener("click", event => {
-        event.preventDefault();
-
-        localStorage.removeItem("userId");
-        localStorage.removeItem("auth");
-        window.location.reload();
-    });
+    localStorage.removeItem("token");
+    window.location.reload();
+  });
 }
--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,6 +1,7 @@
-const alredyLoggedError = document.querySelector(".alredyLogged__error"); 
-const loginEmailError = document.querySelector(".loginEmail__error"); 
-const loginMdpError = document.querySelector(".loginMdp__error"); 
+const alredyLoggedError = document.querySelector(".alredyLogged__error");
+const loginEmailError = document.querySelector(".loginEmail__error");
+const loginMdpError = document.querySelector(".loginMdp__error");
+const logoutButton = document.getElementById("login");
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -11,73 +12,94 @@ alredyLogged();
 
 // Si l'utilisateur est déjà connecté, on supprime le token
 function alredyLogged() {
-    if (localStorage.getItem("token")) {
-        localStorage.removeItem("token");
+  if (localStorage.getItem("token")) {
+    localStorage.removeItem("token");
 
-        const p = document.createElement("p");
-        p.innerHTML = "<br><br><br>Vous avez été déconnecté, veuillez vous reconnecter";
-        alredyLoggedError.appendChild(p);
-        return;
-    }
+    const p = document.createElement("p");
+    p.innerHTML =
+      "<br><br><br>Vous avez été déconnecté, veuillez vous reconnecter";
+    alredyLoggedError.appendChild(p);
+    return;
+  }
 }
 
 // Au clic, on envoie les valeurs de connextion
-submit.addEventListener("click", () => {
-    let user = {
-        email: email.value,
-        password: password.value
-    };
-    login(user);
-})
+submit?.addEventListener("click", () => {
+  let user = {
+    email: email.value,
+    password: password.value,
+  };
+  login(user);
+});
 
+// Au clic sur le bouton de déconnexion
+logoutButton.addEventListener("click", () => {
+  // Supprimer le token du stockage local
+  localStorage.removeItem("token");
+  // Rediriger l'utilisateur vers la page de connexion
+  window.location.href = "login.html";
+});
+
+// Vérifier si l'utilisateur est déjà connecté lors du chargement de la page
+window.onload = function () {
+  if (localStorage.getItem("token")) {
+    // Cacher le bouton de connexion
+    submit.style.display = "none";
+    // Afficher le bouton de déconnexion
+    logoutButton.style.display = "block";
+    logoutButton.textContent = "login";
+  } else {
+    // Cacher le bouton de déconnexion
+    logoutButton.style.display = "none";
+    // Afficher le bouton de connexion
+    submit.style.display = "block";
+  }
+};
 // Fonction de connexion
 function login(id) {
-    console.log(id);
-    loginEmailError.innerHTML = "";
-    loginMdpError.innerHTML = "";
-    // véeification de l'email
-    if (!id.email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/g)) {
+  console.log(id);
+  loginEmailError.innerHTML = "";
+  loginMdpError.innerHTML = "";
+  // véeification de l'email
+  if (!id.email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/g)) {
+    const p = document.createElement("p");
+    p.innerHTML = "Veuillez saisir une addresse mail valide";
+    loginEmailError.appendChild(p);
+    return;
+  }
+  // vérifcation du mot de passe
+  if (id.password.length < 5 && !id.password.match(/^[a-zA-Z0-9]+$/g)) {
+    const p = document.createElement("p");
+    p.innerHTML = "Veuillez saisir un mot de passe valide";
+    loginMdpError.appendChild(p);
+    return;
+  }
+
+  // verification de l'email et du mot de passe
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(id),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      // Si couple email/mdp incorrect
+      if (result.error || result.message) {
         const p = document.createElement("p");
-        p.innerHTML = "Veuillez saisir une addresse mail valide";
-        loginEmailError.appendChild(p);
-        return;
-    }
-    // vérifcation du mot de passe
-    if (id.password.length < 5 && !id.password.match(/^[a-zA-Z0-9]+$/g)) {
-        const p = document.createElement("p");
-        p.innerHTML = "Veuillez saisir un mot de passe valide";
+        p.innerHTML =
+          "La combinaison de l'adresse e-mail et du mot de passe n'est pas valide";
         loginMdpError.appendChild(p);
-        return;
-    }
-
-    else {
-    // verification de l'email et du mot de passe
-    fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(id)
-    })
-    .then(response => response.json())
-    .then(result => { 
-        console.log(result);
-        // Si couple email/mdp incorrect
-        if (result.error || result.message) {
-            const p = document.createElement("p");
-            p.innerHTML = "La combinaison de l'adresse e-mail et du mot de passe n'est pas valide";
-            loginMdpError.appendChild(p);
-
-        // Si couple email/mdp correct
-        } else if (result.token) {
-            localStorage.setItem("token", result.token);
-            window.location.href = "index.html";
-        }
-    
+      } else if (result.token) {
+        localStorage.setItem("token", result.token);
+        window.location.href = "index.html"; // Redirection vers la page principale en cas de succès
+      }
     })
     // prevenir l'utilisateur en cas d'erreur
-    
-    .catch(error => 
-        console.log(error));
-}
+
+    .catch((error) => {
+      console.log(error);
+    });
 }
